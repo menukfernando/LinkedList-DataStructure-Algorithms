5cded95df0a42ec48f2c82a56dd9926f96e04874
menukfernando <100038759+menukfernando@users.noreply.github.com>
2024-08-26 17:46:49 +0530

    Initial commit

---
+++ src/LinkedList.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Menuk Fernando
+ */
+public class LinkedList {
+    Node head;
+    
+    public void Insert(int data){
+        Node newNode = new Node(data);
+        
+        if(head == null){
+            head = newNode;
+        }else{
+            Node currentNode = head;
+            while(currentNode.next!=null){
+                currentNode = currentNode.next;
+            }
+            currentNode.next = newNode;
+        }
+    }
+    
+    public void Show(){
+        Node node = head;
+        while(node.next!=null){
+            System.out.print(node.data + " -> ");
+            node = node.next;
+        }
+        System.out.println(node.data);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Menuk Fernando
+ */
+public class Main {
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+        
+        ll.Insert(10);
+        ll.Insert(20);
+        ll.Insert(30);
+        ll.Insert(40);
+        ll.Insert(50);
+        ll.Insert(60);
+        
+        ll.Show();
+    }
+}
